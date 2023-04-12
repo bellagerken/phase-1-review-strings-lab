6277790b8614e1ce70bc214a1d688676dfa51908
Bella Gerken <bellagerkenn@gmail.com>
2023-04-12 16:47:16 -0400

    complete review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = 'bella'
+
+
+
+let welcomeMessage = 'Welcome to Flatbook, ' + 'bella!'
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'BELLA!'
+
+let shortGreeting = 'Welcome, ' + 'b' + '!'
